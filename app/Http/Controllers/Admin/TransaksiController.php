3a0fa299a51c78dfd6806e20d0234176e70b5df2
Hanapibagas <hanafibagas4@gmail.com>
2023-06-12 16:11:27 +0800

    .

--- app/Http/Controllers/Admin/TransaksiController.php
+++ app/Http/Controllers/Admin/TransaksiController.php
@@ -57,29 +57,13 @@ class TransaksiController extends Controller
 
         dd($total);
 
-<<<<<<< HEAD
         $selectedItems = [];
         foreach ($bahanBakuIds as $bahanBakuId) {
             if (isset($quantities[$bahanBakuId])) {
                 $selectedItems[$bahanBakuId] = $quantities[$bahanBakuId];
             }
-=======
-    $bahanBakuIds = $request->input('bahan_baku');
-    $quantities = $request->input('quantity');
-    $subtotals = $request->input('subtotal');
-
-    $selectedItems_qty = [];
-    $selectedItems_sub = [];
-    foreach ($bahanBakuIds as $bahanBakuId) {
-        if (isset($quantities[$bahanBakuId])) {
-            $selectedItems_qty[$bahanBakuId] = $quantities[$bahanBakuId];
-        }
-        if (isset($subtotals[$bahanBakuId])) {
-            $selectedItems_sub[$bahanBakuId] = $subtotals[$bahanBakuId];
->>>>>>> e3e05f7ae74cbe2470e0e6b98cc049316c247229
         }
 
-<<<<<<< HEAD
         $data = [];
         foreach ($selectedItems as $bahanBakuId => $quantity) {
             $bahanBaku = BahanBaku::find($bahanBakuId);
@@ -89,58 +73,37 @@ class TransaksiController extends Controller
         for ($i = 0; $i < count($request->bahan_baku); $i++) {
             $bahan_baku = BahanBaku::findOrFail($request->bahan_baku[$i]);
             $suplier[] = $bahan_baku->users->id;
-=======
-    $data_qty = [];
-    foreach ($selectedItems_qty as $bahanBakuId => $quantity) {
-        $bahanBaku = BahanBaku::find($bahanBakuId);
-        $data_qty[] = ['bahan_baku' => $bahanBakuId , 'quantity' => $quantity];
-    }
 
-    $data_sub = [];
-    foreach ($selectedItems_sub as $bahanBakuId => $subtotal) {
-        $bahanBaku = BahanBaku::find($bahanBakuId);
-        $supplierId = $bahanBaku->users->id;
-        if (!isset($data_sub[$supplierId])) {
-            $data_sub[$supplierId] = $subtotal;
-        } else {
-            $data_sub[$supplierId] += $subtotal;
->>>>>>> e3e05f7ae74cbe2470e0e6b98cc049316c247229
-        }
-    }
+        $data_qty = [];
+        foreach ($selectedItems_qty as $bahanBakuId => $quantity) {
+            $bahanBaku = BahanBaku::find($bahanBakuId);
+            $data_qty[] = ['bahan_baku' => $bahanBakuId , 'quantity' => $quantity];
 
-    $suplier = [];
-    for($i=0; $i < count($request->bahan_baku); $i++){
-        $bahan_baku = BahanBaku::findOrFail($request->bahan_baku[$i]);
-        $suplier[] = $bahan_baku->users->id;
-    }
+        $data_sub = [];
+        foreach ($selectedItems_sub as $bahanBakuId => $subtotal) {
+            $bahanBaku = BahanBaku::find($bahanBakuId);
+            $supplierId = $bahanBaku->users->id;
+            if (!isset($data_sub[$supplierId])) {
+                $data_sub[$supplierId] = $subtotal;
+            } else {
+                $data_sub[$supplierId] += $subtotal;
+            $transaksi->jumlah_bayar = 0;
+            $transaksi->bukti_bayar = 0;
+            $transaksi->keterangan = '-';
+            $transaksi->total = $data_sub[$supplierId];
+            $transaksi->save();
 
-    // Insert transaksi
-    $uniqueSuppliers = array_unique($suplier);
-
-    foreach ($uniqueSuppliers as $supplierId) {
-        $transaksi = new Transaksi();
-        $transaksi->tgl_pemesanan = date('Y-m-d');
-        $transaksi->jumlah_bayar = 0;
-        $transaksi->bukti_bayar = 0;
-        $transaksi->keterangan = '-';
-        $transaksi->total = $data_sub[$supplierId];
-        $transaksi->save();
-
-        // Insert detail transaksi berdasarkan bahan baku
-        for ($i = 0; $i < count($request->bahan_baku); $i++) {
-            $bahan_baku = BahanBaku::findOrFail($request->bahan_baku[$i]);
-            if ($bahan_baku->users->id == $supplierId) {
-                $detailTransaksi = new DetailTransaksi();
-                $detailTransaksi->transaksi_id = $transaksi->id;
-                $detailTransaksi->bahanbaku_id = $bahan_baku->id;
-                $detailTransaksi->jumlah = $data_qty[$i]['quantity'];
-                $detailTransaksi->save();
+            // Insert detail transaksi berdasarkan bahan baku
+            for ($i = 0; $i < count($request->bahan_baku); $i++) {
+                $bahan_baku = BahanBaku::findOrFail($request->bahan_baku[$i]);
+                if ($bahan_baku->users->id == $supplierId) {
+                    $detailTransaksi = new DetailTransaksi();
+                    $detailTransaksi->transaksi_id = $transaksi->id;
+                    $detailTransaksi->bahanbaku_id = $bahan_baku->id;
+                    $detailTransaksi->jumlah = $data_qty[$i]['quantity'];
+                    $detailTransaksi->save();
+                }
             }
         }
     }
-<<<<<<< HEAD
-=======
-}
-
->>>>>>> e3e05f7ae74cbe2470e0e6b98cc049316c247229
 }
