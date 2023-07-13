482aa379558569f971f2ac88d01ca83f87e037c9
Hanapibagas <hanafibagas4@gmail.com>
2023-07-13 10:18:44 +0800

    .

--- app/Http/Controllers/Admin/laporanController.php
+++ app/Http/Controllers/Admin/laporanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\ProductLayak;
+use App\Models\Transaksi;
 use Illuminate\Http\Request;
 
 class laporanController extends Controller
@@ -11,7 +12,8 @@ class laporanController extends Controller
     public function getIndex()
     {
         $layak = ProductLayak::orderBy('created_at', 'desc')->get();
+        $transaksi = Transaksi::all();
 
-        return view('component.admin.laporan.index', compact('layak'));
+        return view('component.admin.laporan.index', compact('layak', 'transaksi'));
     }
 }
--- resources/views/component/admin/laporan/index.blade.php
+++ resources/views/component/admin/laporan/index.blade.php
@@ -43,6 +43,53 @@ Product Expired
             </div>
         </div>
     </div>
+    <div class="container-fluid">
+        <div class="title-wrapper pt-30">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <div class="title mb-30">
+                        <h2>Data laporan transaksi </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tables-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card-style mb-30">
+                        <div class="table-responsive">
+                            <table id="table" class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Tanggal</th>
+                                        <th>Status</th>
+                                        <th>Jumlah Bayar</th>
+                                        <th>Total</th>
+                                        <th>Keterangan</th>
+                                        <th>Bukti Bayar</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($transaksi as $data)
+                                    <tr>
+                                        <td>{{ $data->tgl_pemesanan }}</td>
+                                        <td>{{ $data->status }}</td>
+                                        <td>Rp.{{ number_format($data->jumlah_bayar) }}</td>
+                                        <td>Rp.{{ number_format($data->total) }}</td>
+                                        <td>{{ $data->keterangan }}</td>
+                                        <td>
+                                            <img src="{{ Storage::url($data->bukti_bayar) }}" alt=""
+                                                style="width: 150px" class="img-thumbnail">
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 @endsection
 
